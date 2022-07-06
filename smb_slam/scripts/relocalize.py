8c271a52f69b1d28a27293f68385d45054e234d1
Chuanfang Ning <chuanfang.ning@epfl.ch>
2022-07-06 07:05:54 -0700

    use 2nd 2D estimate for re-loc

--- smb_slam/scripts/relocalize.py
+++ smb_slam/scripts/relocalize.py
@@ -7,26 +7,25 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 class Relocalize:
     def __init__(self):
         rospy.init_node('challenge_tf_broadcaster')
-        self.received = False
+        self.received = 0
         self.initialpose_subscriber = rospy.Subscriber('initialpose', PoseWithCovarianceStamped, self.get_initial_pose_cb)
         self.new_tf = PoseStamped()
         self.br = tf.TransformBroadcaster()
-        self.listener = tf.TransformListener()
         pass
 
     def get_initial_pose_cb(self,initial_pose_stamped):
-        if not self.received:
-            self.new_tf.pose.position = initial_pose_stamped.pose.pose.position
-            self.new_tf.pose.orientation = initial_pose_stamped.pose.pose.orientation
-            self.received=True
-            rospy.loginfo('Get new tf from 2D pose estimate. Publishing...')
-            trans = Listener
+        if self.received < 2:
+            self.received += 1
+            if self.received == 2:
+                self.new_tf.pose.position = initial_pose_stamped.pose.pose.position
+                self.new_tf.pose.orientation = initial_pose_stamped.pose.pose.orientation
+                rospy.loginfo('Get new tf from 2D pose estimate. Publishing...')
         return 0
 
     def run(self):
         rate = rospy.Rate(10.0)
         while not rospy.is_shutdown():
-            if self.received:
+            if self.received >= 2:
                 self.br.sendTransform((self.new_tf.pose.position.x, self.new_tf.pose.position.y, self.new_tf.pose.position.z),
                                     (self.new_tf.pose.orientation.x, self.new_tf.pose.orientation.y, self.new_tf.pose.orientation.z, self.new_tf.pose.orientation.w),
                                     rospy.Time.now(),
